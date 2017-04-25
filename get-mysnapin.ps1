a29fdb701147c3368e56ca3947edaf3347db5a00
Alain Assaf <alainassaf@outlook.com>
2017-04-25 15:27:02 -0400

    Update get-mysnapin.ps1
    Added help.

--- get-mysnapin.ps1
+++ get-mysnapin.ps1
@@ -1,4 +1,30 @@
 Function Get-MySnapin {
+<#
+    .SYNOPSIS
+    Checks for a PowerShell Snapin(s) and imports it if available, otherwise it will display a warning and exit.
+    .DESCRIPTION
+    Checks for a PowerShell Snapin(s) and imports it if available, otherwise it will display a warning and exit.
+    .PARAMETER snapins
+    Required parameter. List of PSSnapins separated by commas.
+    .INPUTS
+    None
+    .OUTPUTS
+    None
+    .EXAMPLE
+    PS> get-MySnapin PSSNAPIN
+    Checks system for installed PSSNAPIN and imports it if available.
+    .LINK
+    http://www.linkedin.com/in/alainassaf/
+    http://wagthereal.com
+    https://github.com/alainassaf/get-mysnapin
+    .NOTES
+    NAME        :  Get-MySnapin
+    VERSION     :  1.00
+    CHANGE LOG - Version - When - What - Who
+    1.00 - 02/13/2017 - Initial script - Alain Assaf
+    LAST UPDATED:  02/13/2017
+    AUTHOR      :  Alain Assaf
+#>
     Param([string]$snapins)
         $ErrorActionPreference= 'silentlycontinue'
         foreach ($snap in $snapins.Split(",")) {
